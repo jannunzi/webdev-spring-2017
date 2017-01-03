3fe35891b21e3ce80dfad8677058cb7b8facfc6d
Jose Annunziato <jannunzi@gmail.com>
2017-01-03 14:24:42 -0500

    first deploy

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+
+var passport      = require('passport');
+var cookieParser  = require('cookie-parser');
+var session       = require('express-session');
+
+app.use(session({
+    secret: 'this is the secret',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.set('view engine', 'ejs');
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json({type: 'website/json'}));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + '/public'));
+
+// require ("./test/app.js")(app);
+
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+app.listen(port);
