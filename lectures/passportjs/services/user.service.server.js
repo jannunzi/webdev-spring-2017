86e3ea60dec5f24ca7c1dbffed64a1342b2b8770
Jose Annunziato <jannunzi@gmail.com>
2017-03-28 16:21:33 -0400

    passport js morning lecture

--- lectures/passportjs/services/user.service.server.js
+++ lectures/passportjs/services/user.service.server.js
@@ -6,6 +6,9 @@ passport.serializeUser(serializeUser);
 passport.deserializeUser(deserializeUser);
 
 app.post('/api/lecture-morning/login', passport.authenticate('local'), login);
+app.post('/api/lecture-morning/loggedin', loggedin);
+app.post('/api/lecture-morning/logout', logout);
+app.post('/api/lecture-morning/register', register);
 
 var userModel = require('../models/user.model.server');
 
@@ -25,9 +28,33 @@ function localStrategy(username, password, done) {
         );
 }
 
+function register(req, res) {
+    var user = req.body;
+    userModel
+        .createUser(user)
+        .then(function (user) {
+            req.login(user, function (err) {
+                if(err) {
+                    res.send(400)
+                } else {
+                    res.json(user);
+                }
+            });
+        });
+}
+
 function login(req, res) {
     var user = req.user;
     res.json(user);
+}
+
+function loggedin(req, res) {
+    res.send(req.isAuthenticated() ? req.user : '0');
+}
+
+function logout(req, res) {
+    req.logout();
+    res.send(200);
 }
 
 function serializeUser(user, done) {
